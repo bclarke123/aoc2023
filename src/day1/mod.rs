4b906169354fc80fa40ce6933cd078b496d64b6f
Ben Clarke <ben@arrayofstars.com>
2023-12-02 22:50:18 -0500

    Day 1

---
+++ src/day1/mod.rs
@@ -0,0 +1,65 @@
+fn get_sum(input: &str) {
+    let sum = input
+        .lines()
+        .map(|l| l.chars().filter_map(|c| c.to_digit(10)).collect::<Vec<_>>())
+        .fold(0, |acc, cl| {
+            acc + cl.first().unwrap() * 10 + cl.last().unwrap()
+        });
+
+    println!("{:?}", sum);
+}
+
+pub fn part1() {
+    let input = include_str!("input.txt");
+    get_sum(input);
+}
+
+pub fn get_sum2(input: &str) -> u32 {
+    let words = [
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
+    let mut first = 0;
+    let mut last = 0;
+    let mut idx = 0;
+
+    let chars = input.chars().collect::<Vec<_>>();
+    let mut numbers = vec![];
+
+    loop {
+        if idx >= input.len() {
+            break;
+        }
+
+        let next = chars[idx];
+        if next.is_digit(10) {
+            numbers.push(next.to_digit(10).unwrap());
+        }
+
+        let sub = &input[idx..];
+
+        for i in 0..words.len() {
+            if sub.starts_with(&words[i]) {
+                numbers.push(i as u32);
+            }
+        }
+
+        idx += 1;
+    }
+
+    first = numbers[0];
+    last = numbers[numbers.len() - 1];
+
+    return first * 10 + last;
+}
+
+pub fn part2() {
+    let str = include_str!("input.txt");
+    let mut total = 0;
+
+    for line in str.lines() {
+        total += get_sum2(&line);
+    }
+
+    println!("{}", total);
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use aoc_2023::day1;
+
+fn main() {
+    day1::part1();
+    day1::part2();
+}
